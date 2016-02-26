ec1e0f7a0e217d4460d512d10ae3040219142e04
Jan Dąbroś <jsd@semihalf.com>
2016-02-26 13:27:17 +0100

    Improve build script for Armada7040 platforms
    This patch provides set of changes regarding edk2-build.sh script:
    
     - Separate Apn806 and Armada7040_rz
    
    Output files now are placed in separate directories under Build directory.
    Complete UEFI images are named according to platfom name.
    
     - Remove including SEC phase binary into Armada7040 platorms output file
    
    SEC phase is no longer built for Armada7040 Platforms, so it cannot be
    included into final output file.
    Image created for purpose of burning into flash is no longer used, because
    this binary file is created with ATF build system.
    
     - Update build script with "toolchain" option
    
    Beforehand build script supported only GCC48. This patch implements possibilty
    of using all toolchain version supported by edk2 build tools (GCC45-GCC49).
    
     - Change in directory naming from Armada7040->Armada
    
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Jan Dabros <jsd@semihalf.com>

--- edk2-build.sh
+++ edk2-build.sh
@@ -3,17 +3,18 @@
 usage()
 {
 	printf "\nUsage: edk2-build.sh -b <target> -p <platform>\n";
-	printf "Example: ./edk2-build.sh -b DEBUG -p Armada7040\n";
+	printf "Example: ./edk2-build.sh -b DEBUG -p Armada7040 -t GCC48\n";
 	printf "\n";
 	printf "Mandatory parameters:\n";
 	printf "\t-b\tTarget type. Accepts:\tDEBUG,RELEASE\n";
-	printf "\t-p\tPlatform name. Accepts:\tArmada7040\n";
+	printf "\t-p\tPlatform name. Accepts:\tArmada7040_rz, Apn806\n";
+	printf "\t-t\tToolchain version. Accepts:\tGCC45, GCC46 ... GCC49\n";
 
 	printf "Optional parameters:\n";
 	printf "\t-c\tClean build. remove all build artifacts)\n";
 	printf "\n";
 	printf "Environment Variables:\n";
-	printf "\tGCC48_AARCH64_PREFIX     Cross compiler to build EDK2\n";
+	printf "\t<GCCver>_AARCH64_PREFIX     Cross compiler to build EDK2\n";
 	printf "\n";
 	exit 1;
 }
@@ -21,7 +22,7 @@ usage()
 target=0
 platform=0
 clean=NO
-toolchain=GCC48
+toolchain=0
 
 # read all input arguments
 while [[ $# > 0 ]]
@@ -31,6 +32,10 @@ key="$1"
 case $key in
 	-p|--platform)
 	platform="$2"
+	shift # past argument
+	;;
+	-t|--toolchain)
+	toolchain="$2"
 	shift # past argument
 	;;
 	-b|--target)
@@ -61,25 +66,42 @@ if [ "$target" -eq 0 ]; then
 	usage
 fi
 
-if [ -z "$GCC48_AARCH64_PREFIX" ]; then
-	printf " *** Error: Please set environment variables GCC48_AARCH64_PREFIX\n";
+if [ "$toolchain" -eq 0 ]; then
+	printf "\n *** Error: Please set toolchain version (GCC45 - GCC49)\n"
+	usage
+fi
+
+case $toolchain in
+	GCC45);&
+	GCC46);&
+	GCC47);&
+	GCC48);&
+	GCC49);;
+	*)
+	printf "\n *** Error: Unsupported toolchain \"$toolchain\"\n"
+	usage
+	;;
+esac
+toolchain_env="${toolchain}_AARCH64_PREFIX"
+
+if [ -z "${!toolchain_env}" ]; then
+	printf " *** Error: Please set environment variable ${toolchain_env}\n";
 	usage
 fi
 
 dsc_file=0
 output_dir=0
 bin_file=0
-boot_file=0
 bin_ext_file=0
 # Locate the DSC file
 case $platform in
-	Armada7040)
-	plat_dir="OpenPlatformPkg/Platforms/Marvell/Armada7040/"
-	dsc_file="$plat_dir/Armada7040.dsc"
-	bin_ext_file="$plat_dir/Binary/u-boot-spl.bin"
-	output_dir="Build/Armada7040/${target}_$toolchain/FV"
-	bin_file="${output_dir}/ARMADA7040_EFI.bin"
-	boot_file="${output_dir}/ARMADA7040_EFI.img"
+	Apn806);&
+	Armada7040_rz)
+	plat_dir="OpenPlatformPkg/Platforms/Marvell/Armada/"
+	dsc_file="$plat_dir/${platform}.dsc"
+	bin_ext_file="$plat_dir/Binary/${platform}-spl.bin"
+	output_dir="Build/${platform}/${target}_$toolchain/FV"
+	bin_file="${output_dir}/${platform}_EFI.bin"
 	;;
 	*)
 	printf "\n *** Error: Unknown platform \"$platform\"\n"
@@ -92,7 +114,7 @@ printf "\n##### [Setting up Environment]\t#####\n\n";
 printf "Platform	= ${platform}\n"
 printf "Target		= ${target}\n"
 printf "Clean build	= ${clean}\n"
-printf "Compiler	= ${GCC48_AARCH64_PREFIX}\n"
+printf "Compiler	= ${toolchain_env}\n"
 
 source edksetup.sh
 if [ $? -ne 0 ]; then
@@ -121,17 +143,10 @@ fi
 
 printf "\n##### [Building Output files ]\t#####\n\n";
 rm -rf $bin_file
-cat $output_dir/ARMADA7040_EFI_SEC.fd $output_dir/ARMADA7040_EFI.fd > $bin_file
+mv $output_dir/ARMADA7040_EFI.fd $bin_file
 if [ $? -ne 0 ]; then
 	printf "**** Error: Failed creating binary file\n"
 	exit 1
 fi
 printf "Binary image:\t$bin_file\n";
-
-OpenPlatformPkg/Platforms/Marvell/Tools/doimage -b $bin_ext_file $bin_file $boot_file
-if [ $? -ne 0 ]; then
-	printf "**** Error: Failed creating boot image\n"
-	exit 1
-fi
-printf "Boot image:\t$boot_file\n";
 
